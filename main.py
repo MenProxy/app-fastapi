e6f35f0d9885c66c58907acac536ecc1d6ea2d7f
MenProxy <84359906+MenProxy@users.noreply.github.com>
2023-12-05 01:28:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import joblib
-
-app = FastAPI()
-
-# Cargar el modelo
-model = joblib.load('modelo_expectativa_vida.joblib')
 
 class Item(BaseModel):
     bmi: float
     total_expenditure: float
+
+app = FastAPI()
+
+# Lista de orígenes permitidos. Puedes ajustar esto a tus necesidades.
+# Podría ser cualquier origen ('*') o una lista de orígenes permitidos.
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://34.207.209.193:8000",
+    "null",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/hi")
 def index():
